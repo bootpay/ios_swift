4a77f18fe2baf45a97f37aa8e6f6351ec0362c50
ehowlsla <ehowlsla@bootpay.co.kr>
2021-06-21 14:25:36 +0900

    설치되어있지 않을때 처리

--- Bootpay/Classes/webview/BootpayWebView.swift
+++ Bootpay/Classes/webview/BootpayWebView.swift
@@ -91,9 +91,14 @@ import WebKit
             startAppToApp(url)
             decisionHandler(.cancel)
         } else if(!url.absoluteString.starts(with: "http")) {
+            if(UIApplication.shared.canOpenURL(url)) {
+                startAppToApp(url)
+            } else {
+                startItunesToInstall(url)
+            }
             startAppToApp(url)
-            decisionHandler(.allow)
-//            decisionHandler(.cancel)
+//            decisionHandler(.allow)
+            decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
         }
@@ -195,6 +200,46 @@ extension BootpayWebView {
             UIApplication.shared.openURL(url)
         }
         #endif
+    }
+    
+    func startItunesToInstall(_ url: URL) {
+        let sUrl = url.absoluteString
+        var itunesUrl = ""
+        if(sUrl.starts(with: "kfc-bankpay")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EB%B1%85%ED%81%AC%ED%8E%98%EC%9D%B4-%EA%B8%88%EC%9C%B5%EA%B8%B0%EA%B4%80-%EA%B3%B5%EB%8F%99-%EA%B3%84%EC%A2%8C%EC%9D%B4%EC%B2%B4-%EA%B2%B0%EC%A0%9C-%EC%A0%9C%EB%A1%9C%ED%8E%98%EC%9D%B4/id398456030"
+        } else if(sUrl.starts(with: "ispmobile")) {
+            itunesUrl = "https://apps.apple.com/kr/app/isp-%ED%8E%98%EC%9D%B4%EB%B6%81/id369125087"
+        } else if(sUrl.starts(with: "hdcardappcardansimclick")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%ED%98%84%EB%8C%80%EC%B9%B4%EB%93%9C/id702653088"
+        } else if(sUrl.starts(with: "shinhan-sr-ansimclick")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EC%8B%A0%ED%95%9C%ED%8E%98%EC%9D%B4%ED%8C%90/id572462317"
+        } else if(sUrl.starts(with: "kb-acp")) {
+            itunesUrl = "https://apps.apple.com/kr/app/kb-pay/id695436326"
+        } else if(sUrl.starts(with: "mpocket.online.ansimclick")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EC%82%BC%EC%84%B1%EC%B9%B4%EB%93%9C/id535125356"
+        } else if(sUrl.starts(with: "lottesmartpay")) {
+            itunesUrl = "https://apps.apple.com/us/app/%EB%A1%AF%EB%8D%B0%EC%B9%B4%EB%93%9C-%EC%95%B1%EC%B9%B4%EB%93%9C/id688047200"
+        } else if(sUrl.starts(with: "lotteappcard")) {
+            itunesUrl = "https://apps.apple.com/us/app/%EB%A1%AF%EB%8D%B0%EC%B9%B4%EB%93%9C-%EC%95%B1%EC%B9%B4%EB%93%9C/id688047200"
+        } else if(sUrl.starts(with: "cloudpay")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%ED%95%98%EB%82%98%EC%9B%90%ED%81%90-%EC%B9%B4%EB%93%9C-%ED%95%98%EB%82%98%EC%B9%B4%EB%93%9C/id427543371"
+        } else if(sUrl.starts(with: "nhappvardansimclick")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EC%98%AC%EC%9B%90%ED%8E%98%EC%9D%B4-nh%EC%95%B1%EC%B9%B4%EB%93%9C/id1177889176"
+        } else if(sUrl.starts(with: "nhallonepayansimclick")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EC%98%AC%EC%9B%90%ED%8E%98%EC%9D%B4-nh%EC%95%B1%EC%B9%B4%EB%93%9C/id1177889176"
+        } else if(sUrl.starts(with: "citispay")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EC%94%A8%ED%8B%B0%EB%AA%A8%EB%B0%94%EC%9D%BC/id1179759666"
+        } else if(sUrl.starts(with: "payco")) {
+            itunesUrl = "https://apps.apple.com/kr/app/payco-%ED%8E%98%EC%9D%B4%EC%BD%94-%ED%98%9C%ED%83%9D%EA%B9%8C%EC%A7%80-%EB%98%91%EB%98%91%ED%95%9C-%EA%B0%84%ED%8E%B8%EA%B2%B0%EC%A0%9C/id924292102"
+        } else if(sUrl.starts(with: "naversearchapp")) {
+            itunesUrl = "https://apps.apple.com/kr/app/%EB%84%A4%EC%9D%B4%EB%B2%84-naver/id393499958"
+        }
+        
+        if(itunesUrl.count > 0) {
+            if let appstore = URL(string: itunesUrl) {
+                startAppToApp(appstore)
+            }
+        }
     }
     
     func isMatch(_ urlString: String, _ pattern: String) -> Bool {
